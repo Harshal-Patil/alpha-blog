4b0125fab7de573857fec25356ff303f3f8b7307
harshal patil <harshpatilheros@gmail.com>
2021-09-27 16:30:30 +0530

    Authantications and associations are done

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,9 +1,12 @@
 class ArticlesController < ApplicationController
 	before_action :set_article, only: [:show, :edit, :update, :destroy]
+	before_action :require_user, except: [:show, :index]
+	before_action :require_same_user, only: [:edit, :update, :destroy]
+  
 	def show
 	end
 	def index
-		@article=Article.all
+		@articles=Article.all
 	end
 	def new
 		@article = Article.new
@@ -12,7 +15,7 @@ class ArticlesController < ApplicationController
 	end
 	def create
                  @article = Article.new(article_params)
-				 @article.user = User.first
+				 @article.user = current_user
 		 if @article.save
 			 flash[:notice] = "Article was created successfully."
 			 redirect_to @article 
@@ -38,5 +41,11 @@ class ArticlesController < ApplicationController
 	end
 	def article_params
 		params.require(:article).permit(:tittle, :description)
+	end
+	def require_same_user
+		if current_user != @article.user && !current_user.admin?
+		  flash[:alert] = "You can only edit or delete your own article"
+		  redirect_to @article
+		end
 	end
 end
